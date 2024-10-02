1d3496d8fd7df0ec64e6e3b8591adbe676969db2
maducasanova <maria.casanova@poatek.com>
2024-10-01 23:45:44 -0300

    creates project

---
+++ modular-sample/modular-sample/modular_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  modular_sampleApp.swift
+//  modular-sample
+//
+//  Created by Maria Eduarda on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct modular_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
